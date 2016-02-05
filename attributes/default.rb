934fe3b87e8e103e4e281c4e7d0f69c13b5f25c8
tpetchel <tpetchel@gmail.com>
2016-02-05 15:53:11 -0500

    Web user

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default['firewall']['allow_ssh'] = true
 default['awesome_customers_ubuntu']['open_ports'] = 80
+
+default['awesome_customers_ubuntu']['user'] = 'web_admin'
+default['awesome_customers_ubuntu']['group'] = 'web_admin'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,4 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'apt::default'
 include_recipe 'awesome_customers_ubuntu::firewall'
+include_recipe 'awesome_customers_ubuntu::web_user'
