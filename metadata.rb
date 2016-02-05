00713cdd58d6994a2912317e2453280b2d472518
tpetchel <tpetchel@gmail.com>
2016-02-05 13:00:42 -0500

    Initial

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'awesome_customers_ubuntu'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures awesome_customers_ubuntu'
+long_description 'Installs/Configures awesome_customers_ubuntu'
+version '0.1.0'
+
+depends 'apt', '~> 2.9.2'
---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'awesome_customers_ubuntu::default' do
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+  it 'does something' do
+    skip 'Replace this with meaningful tests'
+  end
+end
