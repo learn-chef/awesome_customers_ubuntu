e870056b8be342510310889827d546eb15534a9e
tpetchel <tpetchel@gmail.com>
2016-02-10 23:10:21 -0500

    Database & PHP config

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,11 @@ default['awesome_customers_ubuntu']['open_ports'] = 80
 default['awesome_customers_ubuntu']['user'] = 'web_admin'
 default['awesome_customers_ubuntu']['group'] = 'web_admin'
 default['awesome_customers_ubuntu']['document_root'] = '/var/www/customers/public_html'
+
+default['awesome_customers_ubuntu']['secret_file'] = '/etc/chef/encrypted_data_bag_secret'
+
+default['awesome_customers_ubuntu']['database']['dbname'] = 'my_company'
+default['awesome_customers_ubuntu']['database']['host'] = '127.0.0.1'
+default['awesome_customers_ubuntu']['database']['root_username'] = 'root'
+default['awesome_customers_ubuntu']['database']['admin_username'] = 'db_admin'
+default['awesome_customers_ubuntu']['database']['create_tables_script'] ='/tmp/create-tables.sql'
--- recipes/web.rb
+++ recipes/web.rb
@@ -21,10 +21,28 @@ directory node['awesome_customers_ubuntu']['document_root'] do
   recursive true
 end
 
+# Load the secrets file and the encrypted data bag item that holds the root password.
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['awesome_customers_ubuntu']['secret_file'])
+password_data_bag_item = Chef::EncryptedDataBagItem.load('database_passwords', 'mysql_customers', password_secret)
+
 # Write the home page.
-file "#{node['awesome_customers_ubuntu']['document_root']}/index.html" do
-  content '<html>This is a placeholder</html>'
+template "#{node['awesome_customers_ubuntu']['document_root']}/index.php" do
+  source 'index.php.erb'
   mode '0644'
   owner node['awesome_customers_ubuntu']['user']
   group node['awesome_customers_ubuntu']['group']
+  variables(
+    :database_password => password_data_bag_item['admin_password']
+  )
+end
+
+# Install the mod_php5 Apache module.
+httpd_module 'php5' do
+  instance 'customers'
+end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[customers]'
 end
