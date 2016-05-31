b9eed32355980faaafaa0e4e46a1a7e01d035b25
tpetchel <tpetchel@gmail.com>
2016-05-30 22:19:38 -0400

    Remove default specification

--- recipes/firewall.rb
+++ recipes/firewall.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'firewall::default'
 
-ports = node.default['awesome_customers_ubuntu']['open_ports']
+ports = node['awesome_customers_ubuntu']['open_ports']
 firewall_rule "open ports #{ports}" do
   port ports
 end
