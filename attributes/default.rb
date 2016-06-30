2ac0134c7676581bb173181e1c12d81a2658d922
tpetchel <tpetchel@gmail.com>
2016-06-30 09:28:09 -0400

    default_unless => normal_unless

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,8 +10,8 @@ default['awesome_customers_ubuntu']['user'] = 'web_admin'
 default['awesome_customers_ubuntu']['group'] = 'web_admin'
 default['awesome_customers_ubuntu']['document_root'] = '/var/www/customers/public_html'
 
-default_unless['awesome_customers_ubuntu']['database']['root_password'] = random_password
-default_unless['awesome_customers_ubuntu']['database']['admin_password'] = random_password
+normal_unless['awesome_customers_ubuntu']['database']['root_password'] = random_password
+normal_unless['awesome_customers_ubuntu']['database']['admin_password'] = random_password
 default['awesome_customers_ubuntu']['database']['dbname'] = 'my_company'
 default['awesome_customers_ubuntu']['database']['host'] = '127.0.0.1'
 default['awesome_customers_ubuntu']['database']['root_username'] = 'root'
